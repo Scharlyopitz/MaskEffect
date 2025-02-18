1a15f6ed6807e549e3a2d9c0d6b60fea9e7f1b69
Scharly <scharly.opitz@gmail.com>
2025-02-18 11:41:12 +0100

    Ajout transition pour le mouvement du mask

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import useMouseMoove from "./components/useMouseMoove";
+import { motion as m } from "motion/react";
 
 export default function App() {
   const { x, y } = useMouseMoove();
@@ -9,9 +10,15 @@ export default function App() {
         <p>Bienvenue sur la page d'accueil ici on test l'animation mask</p>
       </div>
 
-      <div className="textContainer mask">
+      <m.div
+        animate={{
+          WebkitMaskPosition: `${x}px ${y}px`,
+        }}
+        transition={{ type: "tween", ease: "backOut" }}
+        className="textContainer mask"
+      >
         <p>Révelation au survol du texte</p>
-      </div>
+      </m.div>
     </main>
   );
 }
