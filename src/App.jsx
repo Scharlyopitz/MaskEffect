ab68d2a19c92e44df498e064ea2743198da459b3
Scharly <scharly.opitz@gmail.com>
2025-02-18 11:04:27 +0100

    Ajout du texte

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
 export default function App() {
-  return <></>;
+  return (
+    <main>
+      <div className="textContainer mask">
+        <p>Bienvenue sur la page d'accueil ici on test l'animation mask</p>
+      </div>
+
+      <div className="textContainer mask">
+        <p>Révelation au survol du texte</p>
+      </div>
+    </main>
+  );
 }
